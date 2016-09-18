cc23416e5a5c7b49ebbec8269c5e9125ecd96a08
Gordondalos <gordondalos@gmail.com>
2016-09-18 21:35:21 +0600

    приделал первую страницу

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+    <div ng-controller="PhotoController">
+        <section class="section_2">
+            <div class="container">
+                <div class="row p-y-1" ng-repeat="q in qr| filter:search" ng-class="$odd ? 'odd' : 'even'">
+                    <div class="col-xs-8">
+                        <ul class="list-unstyled">
+                            <li><span>Магазин</span> <b>@{{ q.shop }}</b></li>
+                            <li><span>Клиент</span> <b>@{{ q.client }}</b></li>
+                            <li><span>Номер посылки</span> <b>@{{ q.number }}</b></li>
+                            <li><span>Партнер</span> <b>@{{ q.partner }}</b></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-4 text-xs-center">
+                        <a href="photo_add.html?q=@{{ q.number }}" class="add_button btn btn-primary m-t-2"><i class="fa fa-camera" aria-hidden="true"></i> add</a>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </section>
+
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,80 +3,148 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravelapp</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">--}}
+    {{--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">--}}
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">--}}
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-    <style>
-        body {
-            font-family: 'Lato';
-        }
+    <link rel="shortcut icon" href="{{ asset('build/img/favicon/favicon.ico') }}" type="image/x-icon">
 
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
+
+    <link rel="stylesheet" href="{{ asset('build/css/main.css') }}">
+
+
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+<body ng-app="myApp">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+<section class="section_1 bg-faded">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 p-x-0">
+                <nav class="navbar bavbar-light bg-faded">
+                    <div class="col-sm-1 col-xs-2 p-x-0">
+                        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse"
+                                data-target="#exCollapsingNavbar2" aria-controls="exCollapsingNavbar2" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            &#9776;
+                        </button>
+                    </div>
+                    <div class="col-xs-10 col-sm-12">
+                        <div class="row">
+                            <div class="input-group ">
+                                <input ng-model="search" type="text" class="form-control" placeholder="Search for...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-secondary" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="collapse navbar-toggleable-xs p-t-1" id="exCollapsingNavbar2">
+                        <a class="navbar-brand" href="#">Photo manager</a>
+                        <div class="clearfix hidden-sm-up"></div>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
+                            <!-- Authentication Links -->
+                            @if (Auth::guest())
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                                </li>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/logout') }}">{{ Auth::user()->name }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/logout') }}">Logout</a>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
-    </nav>
+    </div>
+</section>
+
+
+
+
 
     @yield('content')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>--}}
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+
+
+<!--[if lt IE 9]>
+    <script src="{{asset('build/libs/html5shiv/es5-shim.min.js')}}"></script>
+    <script src="{{asset('build/libs/html5shiv/html5shiv.min.js')}}"></script>
+    <script src="{{asset('build/libs/html5shiv/html5shiv-printshiv.min.js')}}"></script>
+    <script src="{{asset('build/libs/respond/respond.min.js')}}"></script>
+    <![endif]-->
+
+
+    <script src="{{asset('build/libs/modernizr/modernizr.js')}}"></script>
+    <script src="{{asset('build/libs/jquery/jquery-1.11.2.min.js')}}"></script>
+    <script src="{{asset('build/libs/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{asset('build/libs/animate/animate-css.js')}}"></script>
+    <script src="{{asset('build/libs/plugins-scroll/plugins-scroll.js')}}"></script>
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+    <script src="{{asset('build/js/vendor/jquery.ui.widget.js')}}"></script>
+    <!-- The Templates plugin is included to render the upload/download listings -->
+    <script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
+    <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+    <script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
+    <!-- The Canvas to Blob plugin is included for image resizing functionality -->
+    <script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+    <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!-- blueimp Gallery script -->
+    <script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+    <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+    <script src="{{asset('build/js/vendor/jquery.iframe-transport.js')}}"></script>
+    <!-- The basic File Upload plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload.js')}}"></script>
+    <!-- The File Upload processing plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload-process.js')}}"></script>
+    <!-- The File Upload image preview & resize plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload-image.js')}}"></script>
+    <!-- The File Upload audio preview plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload-audio.js')}}"></script>
+
+    <!-- The File Upload validation plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload-validate.js')}}"></script>
+    <!-- The File Upload user interface plugin -->
+    <script src="{{asset('build/js/vendor/jquery.fileupload-ui.js')}}"></script>
+
+
+
+    <script src="{{asset('build/js/common.js')}}"></script>
+
+
+
+
+
 </body>
 </html>
